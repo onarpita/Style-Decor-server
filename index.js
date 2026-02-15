5af6f9d78a5f1b304d39d647ba17a83133a40fda
onarpita <onarpita14@gmail.com>
2026-02-15 22:26:44 +0600

    middleware added

--- index.js
+++ index.js
@@ -12,3 +12,19 @@ admin.initializeApp({
 });
 
 const app = express();
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// jwt middlewares
+const verifyJWT = async (req, res, next) => {
+    const token = req?.headers?.authorization?.split(' ')[1];
+    if (!token) return res.status(401).send({ message: 'Unauthorized Access!' });
+    try {
+        const decoded = await admin.auth().verifyIdToken(token);
+        req.tokenEmail = decoded.email;
+        next();
+    } catch (err) {
+        return res.status(401).send({ message: 'Unauthorized Access!', err });
+    }
+}
