e674179f27110aa5731674d5ed353a06d906fee0
onarpita <onarpita14@gmail.com>
2026-02-15 22:25:18 +0600

    update index file

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const admin = require('firebase-admin');
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
+const port = process.env.PORT || 3000;
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf-8');
+const serviceAccount = JSON.parse(decoded);
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
+
+const app = express();
